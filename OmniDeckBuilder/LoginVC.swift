f7c1c1da3d918e3c6ce2e41c3d3a65973215d518
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2018-04-19 17:42:02 -0400

    Added Firebase, Alamofire, Toucan, and removed storyboard

---
+++ OmniDeckBuilder/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  OmniDeckBuilder
+//
+//  Created by Richard Crichlow on 4/19/18.
+//  Copyright © 2018 Richard Crichlow. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    
+    
+    
+    
+}
---
+++ Pods/Target Support Files/Pods-OmniDeckBuilder/Pods-OmniDeckBuilder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OmniDeckBuilder : NSObject
+@end
+@implementation PodsDummy_Pods_OmniDeckBuilder
+@end
